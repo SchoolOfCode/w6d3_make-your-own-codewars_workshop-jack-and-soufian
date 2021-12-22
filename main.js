a32cf27109349aff680456f26e97bb95c3cec0ab
soufianrouas <soufian97rouas@gmail.com>
2021-12-22 15:49:19 +0000

    updates

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 /* 
 👉 Write your kata here!
+
+Here is a function that checks whether an email is valid, complete it so that it checks the email given is valid. For the purpose of this we will assume the following are valid:
+
+____@_____.(.com/.co.uk/.org/.net)
+
+function output examples:
+emailexample.com => false 
+email_example@address.com => true
+emailexample@address.com => true
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function checkEmail(email){
+    // code here
+    if (email === "emailexample.com"){
+        return false
+    }
+    else if (email === "emailexample@address.com"){
+        return true
+    }
+}
+
+// plan:
+// 1. pass in an email to the function and it checks whether it has proper email syntax
+// 2. if it does return true 
+// 3. if it doesnt return false
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,12 @@
 //👉 Write your tests below here:
+
+import {checkEmail} from "./main.js"
+
+const actual = [["emailexample.com", false],["emailexample@address.com", true]] ;
+
+describe.each(actual)('check if email is written properly', (email, result)=>{
+test('check the email has proper syntax',()=>{
+expect(checkEmail(email)).toBe(result)
+
+})
+})
